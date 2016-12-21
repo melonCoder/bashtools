4a922a86d7a3e9c2836294a04f5d7b0d29e08bbf
melonCoder <melonCoder@126.com>
2016-12-21 11:26:35 +0800

    add function gt

--- user_defined_command.sh
+++ user_defined_command.sh
@@ -1,5 +1,9 @@
 #/bin/bash
 
+################################################################################
+##################### vnote: a simple script function ##########################
+##################### make note anywhere you want     ##########################
+################################################################################
 function vnote(){
     #if note directory is not available, create it
     if [ ! -d ~/note ]
@@ -9,6 +13,7 @@ function vnote(){
 
     # default edit a markdown file according the current time
     # or edit a specified file
+    local noteName
     if [ "a$1" == "a" ]
     then
         noteName=`date "+%y-%m-%d"`
@@ -18,3 +23,43 @@ function vnote(){
     fi
     vim ~/note/$noteName
 }
+################################################################################
+
+################################################################################
+##################### gt: a simple script function #############################
+##################### bring you where you wanna go to ##########################
+################################################################################
+# TODO: AUTO IMPLEMENTATION
+# CON: too slow
+function gt(){
+    #set -x
+    if [ "a$1" == "a" ]; then
+        usage_gt
+    fi
+
+    local simple_target=`echo $1 | awk -F '/' '{print $NF}'`
+    local i=0
+    local target_list
+    while read target
+    do
+        target_list[i]=$target
+        let i++
+    done <<< "$(find -type d -iname ${simple_target} | grep -i $1)"
+
+    local target_cnt=${#target_list[@]}
+    if [ "${target_cnt}" -eq 1 ]; then
+        builtin cd $target_list
+    else 
+        echo "There are more than one candidate"
+        for i in $( seq 0 $target_cnt )
+        do
+            echo "    ${target_list[i]}"
+        done
+    fi
+}
+
+function usage_gt(){
+    echo "Usage:"
+    echo "    gt TARGET_DIR"
+}
+################################################################################
